aeec178a5af7cb939ce39b799068885e668b93b6
TugasConditionalStatement_0089 <rajhwaa@gmail.com>
2026-03-14 00:20:42 +0700

    Refactor main function to utilize Rata_rata for average calculation and clean up commented code

--- pertemuan ke 4/pertemuan_4.cpp
+++ pertemuan ke 4/pertemuan_4.cpp
@@ -20,3 +20,11 @@ int main(){
     cin >> nilBI;
     cout << "Masukkan nilai Matematika = ";
     cin >> nilMT;
+
+    //rerata = (nilBI+nilMT)/2;
+    //if (rerata >= 60)
+    //    status = "Lolos";
+    //else
+    //    status = "Gagal";
+    rerata = Rata_rata(nilBI,nilMT);
+    //status = Status_lulus(rerata);
