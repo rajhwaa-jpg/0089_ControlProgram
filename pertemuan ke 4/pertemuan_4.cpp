298a64c4cbcca855cbeba98badb51406c396b81c
TugasConditionalStatement_0089 <rajhwaa@gmail.com>
2026-03-14 00:19:52 +0700

    Add input prompts for user to enter scores in main function

--- pertemuan ke 4/pertemuan_4.cpp
+++ pertemuan ke 4/pertemuan_4.cpp
@@ -15,3 +15,8 @@ string Status_lulus(float r){
 int main(){
     float nilBI,nilMT,rerata;
     string status;
+
+     cout << "Masukkan nilai Bahasa Inggris = ";
+    cin >> nilBI;
+    cout << "Masukkan nilai Matematika = ";
+    cin >> nilMT;
